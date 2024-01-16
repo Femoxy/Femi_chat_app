b3a01bb4c2e16aaa4ac4570e559f0838506f61cb
Femoxy <olufemi261@gmail.com>
2024-01-16 01:16:55 +0100

    simple chat app

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http')
+const socket=require('socket.io')
+const port = 1234
+const app = express()
+const server = http.createServer(app)
+const io = socket(server)
+
+const messageHistory =[];
+app.set("view engine", "ejs")
+
+
+app.get("/", (req, res)=>{
+    res.render("homepage", {messageHistory})
+})
+
+
+server.listen(port, ()=>{
+    console.log('listening on port' +port)
+});
+
+io.on("connection", (socket)=>{
+
+    socket.broadcast.emit('message history', messageHistory);
+    socket.on('message', (data)=>{
+        messageHistory.push(data);
+        socket.broadcast.emit('message', data)
+    })
+})

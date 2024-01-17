4d446e0cbb004d7cd9b873b0a8daae8f0409d67a
Femoxy <olufemi261@gmail.com>
2024-01-17 13:12:26 +0100

    chat

--- server.js
+++ server.js
@@ -6,24 +6,24 @@ const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 
-const messageHistory =[];
+const messageHistory = [];
+
 app.set("view engine", "ejs")
 
-
-app.get("/", (req, res)=>{
-    res.render("homepage", {messageHistory})
+app.get("/", (req, res) => {
+    res.render("homepage", { messageHistory })
 })
 
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
 
     socket.broadcast.emit('message history', messageHistory);
       
-    socket.on('message', (data)=>{
+    socket.on('message', (data) => {
         messageHistory.push(data);
         socket.broadcast.emit('message', data)
     })
 })
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log('listening on port' +port)
 });
